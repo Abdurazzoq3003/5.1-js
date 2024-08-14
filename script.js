484393954d2808fb7360a68188c25969fe8fc9a0
Abdurazzoq3003 <aabdurazzoq80@gmail.com>
2024-08-14 21:50:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting!");
+let familiya = prompt("familiyangizni kiriting!");
+let yosh = prompt("yoshingizni kiriting!");
+let yoqtirgan = prompt("yoqtirgan faningizni kiriting!");
+let yashash = prompt("yashash manzilingizni kiriting!");
+
+console.log("Ism: " + ism);
+console.log("Famimiliya: " + familiya);
+console.log("yosh:" + yosh);
+console.log("yogtirgan fan :" + yoqtirgan);
+console.log("yashash manzil:" + yashash);
